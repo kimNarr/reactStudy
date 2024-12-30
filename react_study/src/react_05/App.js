91d7775019c1683de1a1a74ece9d5b10ce291247
kimNarr <ignitionhero@gmail.com>
2024-12-30 15:50:28 +0900

    quiz useEffect img

--- react_study/src/react_05/App.js
+++ react_study/src/react_05/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ClassCount from './ClassCount'
 import Timer from './Timer'
 import FunctionCount from './FunctionCount'
+import FunctionTimer from './FunctionTimer'
 
 function App() {
   return (
     <div>
       {/* <ClassCount /> */}
       {/* <Timer /> */}
-      <FunctionCount />
+      {/* <FunctionCount /> */}
+      <FunctionTimer />
     </div>
   )
 }
--- react_study/src/react_05/FunctionTimer.js
+++ react_study/src/react_05/FunctionTimer.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function FunctionTimer() {
+    const [count, setCount] = useState(0)
+    const [running, setRunning] = useState(false)
+    let timer = null;
+    
+    function start() {
+        if(!running) {
+            setRunning(true)   
+            timer = setInterval(()=>{
+                setCount((count) => count + 1)
+            },1000)
+        } 
+    }
+    function stop() {
+        setRunning(false);
+        clearInterval(timer)
+        console.log(timer)
+    }
+    function reset() {
+        setCount(0)
+        clearInterval(timer)
+    }
+    
   return (
     <div>
-      
+      <h2>count : {count} 초</h2>
+      <button type='button' onClick={start}>start</button>
+      <button type='button' onClick={stop}>stop</button>
+      <button type='button' onClick={reset}>reset</button>
     </div>
   )
 }
