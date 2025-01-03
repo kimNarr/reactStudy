583f70800a9158d5a6b0cc1ce8824da0cd2b6ed9
kimNarr <ignitionhero@gmail.com>
2025-01-03 15:49:13 +0900

    todolist

--- react_study/src/react_07/List.js
+++ react_study/src/react_07/List.js
@@ -1,20 +1,31 @@
 import React from 'react'
 
-function List({list, setList}) {
+function List({list, setList, setOnList, onList}) {
     const deleteList = (idx) => {
         const newList = list.filter((li, i)=> {
             return idx !== i
         })
         setList(newList)
+        console.log(newList)
+    }
+    
+    const changeCheck = (idx) => {
+        const newList = list.map((li, i)=>({
+            ...li, checked: i === idx ? !li.checked : li.checked,
+        }));
+        setList(newList)
+        console.log(newList)
+        console.log(list)
+
     }
     return (
         <div className='list_wrap'>
         <ul>
-            {list.map((li, idx)=>(
+            {onList.map((li, idx)=>(
                 <li key={idx}>
                     <div className='todolist'>
-                        <input type="checkbox" id={"check" + idx}/>
-                        <label htmlFor={"check" + idx}>{li.text}</label>
+                        <input type="checkbox" id={idx} onChange={()=>{changeCheck(idx)}} checked={li.checked}/>
+                        <label htmlFor={idx}>{li.text}</label>
                         <button type='button' onClick={()=>{deleteList(idx)}}>삭제</button>
                     </div>
                 </li>
--- react_study/src/react_07/Todo.js
+++ react_study/src/react_07/Todo.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import List from './List'
 
 function Todo() {
+    const [text, setText] = useState('');
     const [list, setList] = useState([]);
-    const [todo, setTodo] = useState('');
+    const [onList, setOnList] = useState([]);
+    const [filter, setFilter] = useState('all')
 
     const addText = (e) => {
-      if(e.target.value == " ") {
+      if(e.target.value === " ") {
         alert("공백부터 작성 할 수 없습니다.")
       } else {
-        setTodo(e.target.value)
+        setText(e.target.value)
       }
     }
 
     const addList = () => {
-        if(todo !== "") {
-          const newList = list.concat({
-            text : todo,
-            check : false,
-          })
-          setList(newList)
-          console.log(newList)
-          setTodo('')
-        } else {
+        if(text === "") {
           alert("할 일을 입력하세요")
+        } else {
+          // const newList = list.concat({
+          //   id: list.length,
+          //   text : text,
+          //   checked : false,
+          // })
+          const newList = [...list, {
+            text : text,
+            checked : false,
+          }]
+          setList(newList)
+          setText('')
         }
     }
 
@@ -33,19 +39,30 @@ function Todo() {
         }
     }
 
+    useEffect(()=>{
+      if(filter === "all") {
+        setOnList(list)
+      } else if (filter === "ing") {
+        setOnList(list.filter(item => item.checked === false))
+      } else if (filter === "end") {
+        setOnList(list.filter(item => item.checked === true))
+      }
+    },[list, filter])
+
+
   return (
     <div className='todo_wrap'>
       <div className='input_wrap'>
-        <input type="text" value={todo} onChange={addText} onKeyDown={addListEnter} placeholder='할 일을 입력하세요'/>
+        <input type="text" value={text} onChange={addText} onKeyDown={addListEnter} placeholder='할 일을 입력하세요'/>
         <button type='button' onClick={addList}>+</button>
       </div>
       <div className='filter_wrap'>
-        <button type='button'>전체</button>
-        <button type='button'>할일</button>
-        <button type='button'>완료</button>
+        <button type='button' onClick={()=>setFilter("all")}>전체</button>
+        <button type='button' onClick={()=>setFilter("ing")}>할일</button>
+        <button type='button' onClick={()=>setFilter("end")}>완료</button>
         <button type='button'>완료 삭제</button>
       </div>
-      <List setList={setList} list={list}/>
+      <List setList={setList} list={list} setOnList={setOnList} onList={onList}/>
     </div>
 
   )
